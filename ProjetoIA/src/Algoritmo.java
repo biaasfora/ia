657fab69f7d1b86b9c8ddbc61c49532b1e485575
user <user@10.246.238.135>
2018-02-01 16:29:41 -0200

    Foi

---
+++ ProjetoIA/src/Algoritmo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Algoritmo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Cachorro> listaDeCaes = new ArrayList<Cachorro>();
+		
+	}
+}
---
+++ ProjetoIA/src/Cachorro.java
@@ -0,0 +1,17 @@
+
+public class Cachorro {
+
+	float raca; 
+	float porte;
+	float energia;
+	float brincalhao;
+	float sociavelComCaes;
+	float sociavelComEstranhos;
+	float protecao;
+	float necessidadeDeExercicio;
+	float apegoAoDono;
+	float facilidadeDeTreinamento;
+	float guarda;
+	float higiene;
+	
+}
